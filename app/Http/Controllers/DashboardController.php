3ec466848864f7cb74df41c5182a36a0776b4645
Nahid Hasan <n16103238@gmail.com>
2019-11-17 21:12:45 +0600

    first comit

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+class DashboardController extends Controller
+{
+  public function admin_dashboard()
+  {
+      return view('dashboard.admin_dashboard');
+  }
+
+
+public function salesman_dashboard()
+  {
+      return view('Dashboard.salesman_dashboard');
+  }
+  public function denied()
+  {
+      return view('auth.permissiondenied');
+  }  
+
+
+
+
+}
---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['middleware'=>['auth']],function(){
+  Route::group(['middleware'=>['_admin']],function(){
+    Route::get('/admin_dashboard', 'DashboardController@admin_dashboard')->name('admin_dashboard');
+  });
+});
+Route::group(['middleware'=>['auth']],function(){
+  Route::group(['middleware'=>['_salesman']],function(){
+    Route::get('/salesman_dashboard', 'DashboardController@salesman_dashboard')->name('salesman_dashboard');
+  });
+});
+
+
+//Dashboard
+Route::get('/dashboard/admin_dashboard', 'DashboardController@admin_dashboard')->name('admin_dashboard');
+Route::get('/salesman_dashboard', 'DashboardController@salesman_dashboard')->name('salesman_dashboard');
+Route::get('/denied', 'DashboardController@denied')->name('denied');
